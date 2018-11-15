891e6753954c2825d9400d40ec643c8f60dae934
Crowley <john.crowley@student.fairfield.edu>
2018-11-14 19:25:21 -0500

    Comment/NamingConventions
    Just cleaning up the file and adding some naming conventions as well as removing and adding comments

--- app/src/main/java/com/example/vanda/agrivolution/Registration.java
+++ app/src/main/java/com/example/vanda/agrivolution/Registration.java
@@ -15,75 +15,61 @@ import java.lang.reflect.Array;
 
 public class Registration extends AppCompatActivity implements AdapterView.OnItemSelectedListener
 {
-    //private EditText fname, lname, mob, email, password, confirmpassword;
-    //private Button register;
-    // private TextView login;
-    EditText farmName;
-    EditText farmAddress;
-    EditText yoe;
-    EditText specialization;
-    Spinner myspinner;
+    EditText etxtFarmName;
+    EditText etxtFarmAddress;
+    EditText etxtYOE;
+    EditText etxtSpecialization;
+    Spinner spnUserSelection;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
-
         //Dropdown for the User Type
-        myspinner = (Spinner) findViewById(R.id.spinnerUserType);
-        myspinner.setOnItemSelectedListener(this);
-        ArrayAdapter<String> myadapter = new ArrayAdapter<String>(Registration.this,
-                android.R.layout.simple_list_item_1 , getResources().getStringArray(R.array.type));
+        spnUserSelection = (Spinner) findViewById(R.id.spinnerUserType);
+        spnUserSelection.setOnItemSelectedListener(this);
+        ArrayAdapter<String> myadapter = new ArrayAdapter<String>(Registration.this, android.R.layout.simple_list_item_1 , getResources().getStringArray(R.array.type));
         myadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        myspinner.setAdapter(myadapter);
-        farmName = findViewById(R.id.farmName);
-        farmAddress = findViewById(R.id.farmAddress);
-        yoe = findViewById(R.id.yoe);
-        specialization = findViewById(R.id.specialization);
-
+        spnUserSelection.setAdapter(myadapter);
+        etxtFarmName = findViewById(R.id.farmName);
+        etxtFarmAddress = findViewById(R.id.farmAddress);
+        etxtYOE = findViewById(R.id.yoe);
+        etxtSpecialization = findViewById(R.id.specialization);
         //Dropdown ends
-
-        //login.setOnClickListener(new View.OnClickListener() {
-          //  @Override
-            //public void onClick(View v) {
-              //  Intent intent = new Intent(Registration.this, Login.class);
-                //startActivity(intent);
-            //}
-        //});
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
     {
-        String selected = myspinner.getSelectedItem().toString();
+        String selected = spnUserSelection.getSelectedItem().toString();
         if(selected.equals("Farmer"))
         {
-            specialization.setVisibility(View.GONE);
-            farmName.setVisibility(View.VISIBLE);
-            farmAddress.setVisibility(View.VISIBLE);
-            yoe.setVisibility(View.VISIBLE);
+            etxtSpecialization.setVisibility(View.GONE);
+            etxtFarmName.setVisibility(View.VISIBLE);
+            etxtFarmAddress.setVisibility(View.VISIBLE);
+            etxtYOE.setVisibility(View.VISIBLE);
         }
         else if(selected.equals("Expert"))
         {
-            farmName.setVisibility(View.GONE);
-            farmAddress.setVisibility(View.GONE);
-            yoe.setVisibility(View.GONE);
-            specialization.setVisibility(View.VISIBLE);
-            yoe.setVisibility(View.VISIBLE);
+            etxtFarmName.setVisibility(View.GONE);
+            etxtFarmAddress.setVisibility(View.GONE);
+            etxtYOE.setVisibility(View.GONE);
+            etxtSpecialization.setVisibility(View.VISIBLE);
+            etxtYOE.setVisibility(View.VISIBLE);
         }
         else if (selected.equals("Other"))
         {
-            farmName.setVisibility(View.GONE);
-            farmAddress.setVisibility(View.GONE);
-            specialization.setVisibility(View.GONE);
-            yoe.setVisibility(View.GONE);
+            etxtFarmName.setVisibility(View.GONE);
+            etxtFarmAddress.setVisibility(View.GONE);
+            etxtSpecialization.setVisibility(View.GONE);
+            etxtYOE.setVisibility(View.GONE);
         }
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent)
     {
-
+        // Do Nothing
     }
 
 }
