b74de9aad7b3085d73de86a2853321e9cc6b7902
Unknown <vandana9861@gmail.com>
2018-11-15 12:01:19 -0500

    Added Logout Logic

--- app/src/main/java/com/example/vanda/agrivolution/Dashboard.java
+++ app/src/main/java/com/example/vanda/agrivolution/Dashboard.java
@@ -1,13 +1,27 @@
 package com.example.vanda.agrivolution;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Dashboard extends AppCompatActivity {
 
+    private TextView Logout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        Logout = (TextView)findViewById(R.id.tvLogout);
+
+        Logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Dashboard.this, Login.class);
+                startActivity(intent);
+            }
+        });
     }
 }
