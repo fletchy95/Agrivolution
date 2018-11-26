701fec0aaeba9ceb91f7b52cb58d4881f5e4fffe
Crowley <john.crowley@student.fairfield.edu>
2018-11-26 15:14:18 -0500

    Front/Back end Dashboard
    Updates on both sides.  Need pages to be created/ need further content before I can further this implementation.

--- app/src/main/java/com/example/vanda/agrivolution/Dashboard.java
+++ app/src/main/java/com/example/vanda/agrivolution/Dashboard.java
@@ -1,22 +1,34 @@
 package com.example.vanda.agrivolution;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ImageButton;
 
 public class Dashboard extends AppCompatActivity {
 
     private TextView Logout;
+    ImageButton btnSubmitIssue;
+    ImageButton btnCommunity;
+    ImageButton btnShop;
+    ImageButton btnHeatMap;
+    ImageButton btnEncyclopedia;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-
+        btnCommunity = findViewById(R.id.btnCommunity);
+        btnSubmitIssue = findViewById(R.id.btnSubmitIssue);
+        btnShop = findViewById(R.id.btnShop);
+        btnHeatMap = findViewById(R.id.btnHeatMap);
+        btnEncyclopedia = findViewById(R.id.btnEncyclopedia);
+        // Will setup button Listeners once the pages for these have been created. 
         Logout = (TextView)findViewById(R.id.tvLogout);
-
         Logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -25,5 +37,10 @@ public class Dashboard extends AppCompatActivity {
                 Toast.makeText(Dashboard.this," Logged out Successfully !", Toast.LENGTH_SHORT).show();
             }
         });
+
+    }
+    protected void onClick_Encyclopedia()
+    {
+
     }
 }
