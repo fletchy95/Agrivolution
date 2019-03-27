830ec9d54b12004ae7436015b95ae048e42b0047
Unknown <vandana9861@gmail.com>
2019-03-27 15:43:31 -0400

    Added the logic to select image from the gallery

--- app/src/main/java/com/example/vanda/agrivolution/addEncyclopediaData.java
+++ app/src/main/java/com/example/vanda/agrivolution/addEncyclopediaData.java
@@ -1,13 +1,40 @@
 package com.example.vanda.agrivolution;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class addEncyclopediaData extends AppCompatActivity {
-
+    private ImageButton mSelectImage;
+    private static final int Gallery_Request =1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_encyclopedia_data);
+
+
+        mSelectImage =(ImageButton) findViewById(R.id.imageSelect);
+        mSelectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent,Gallery_Request);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode== Gallery_Request && requestCode == RESULT_OK){
+            Uri imageUri = data.getData();
+            mSelectImage.setImageURI(imageUri);
+        }
     }
 }
