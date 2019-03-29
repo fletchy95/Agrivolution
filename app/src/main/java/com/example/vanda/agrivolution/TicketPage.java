27c148bc836fbdae5d124f509a5833c07416d34f
Fletchy <john.crowley@student.fairfield.edu>
2019-03-29 18:09:09 -0400

    Clean Up
    Cleaning up

--- app/src/main/java/com/example/vanda/agrivolution/TicketPage.java
+++ app/src/main/java/com/example/vanda/agrivolution/TicketPage.java
@@ -14,33 +14,10 @@ import java.util.ArrayList;
 
 public class TicketPage extends ListActivity
 {
-    ArrayList issueList;
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        issueList = new ArrayList();
-        // storing string resources into Array
-        // here you store the array of string you got from the database
-
-        // Binding Array to ListAdapter
-        this.setListAdapter(new ArrayAdapter<String>(this, R.layout.activity_ticket_page, R.id.listView));
-        // refer the ArrayAdapter Document in developer.android.com
-        ListView lv = getListView();
-
-        // listening to single list item on click
-        lv.setOnItemClickListener(new OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
-            {
-                // selected item
-                String num = ((TextView) view).getText().toString();
-
-                // Launching new Activity on selecting single List Item
-                //Intent i = new Intent(getApplicationContext(), SingleListItem.class);
-                // sending data to new activity
-                //i.putExtra("number", num);
-                //startActivity(i);
-            }
-        });
+        setContentView(R.layout.activity_ticket_page);
     }
 }
