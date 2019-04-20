69b4d95dc6ed88abd35a54ff4bdc874ae5900cef
Vandana <vandana.venkatraman@student.fairfield.edu>
2019-04-20 19:39:36 -0400

    Text View when no ticket is created and its visibility is gone when the user creates a new ticket.

--- app/src/main/java/com/example/vanda/agrivolution/TicketPage.java
+++ app/src/main/java/com/example/vanda/agrivolution/TicketPage.java
@@ -20,8 +20,10 @@ import com.firebase.ui.database.SnapshotParser;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 import java.util.HashMap;
@@ -34,15 +36,30 @@ public class TicketPage extends AppCompatActivity
     private RecyclerView mTicketList;
     private String userID;
     private FirebaseAuth firebaseauthObj;
+    private TextView Display;
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ticket_page);
+        Display = findViewById(R.id.tv_display);
         firebaseauthObj = FirebaseAuth.getInstance();
         FirebaseUser user = firebaseauthObj.getCurrentUser();
         userID = user.getUid();
         mDatabase = FirebaseDatabase.getInstance().getReference().child("Tickets").child(userID);
+        mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if(!(dataSnapshot.getValue()==null)){
+                        Display.setVisibility(View.GONE);
+                    }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
         mTicketList = findViewById(R.id.ticket_list);
         mTicketList.setLayoutManager(new LinearLayoutManager(this));
         mTicketList.setHasFixedSize(true);
@@ -141,9 +158,10 @@ public class TicketPage extends AppCompatActivity
                 holder.setStatus(model.getStatus());
                 holder.setUrl(model.getImageURL());
                 holder.setKey(model.getKey());
-                if(model.getStatus().equals("closed")){
+                if(model.getStatus().equals("Closed")){
                 holder.BtnCloseTicket.setVisibility(View.GONE);
             }
+
 
                 holder.root.setOnClickListener(new View.OnClickListener() {
                     @Override
