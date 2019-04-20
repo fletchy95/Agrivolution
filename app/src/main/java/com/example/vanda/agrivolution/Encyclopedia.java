7f580e91aa6692d6813b043348e2d2489398dff9
Vandana <vandana.venkatraman@student.fairfield.edu>
2019-04-19 22:12:50 -0400

    Added Logic to Hide the Add option for the Farmers

--- app/src/main/java/com/example/vanda/agrivolution/Encyclopedia.java
+++ app/src/main/java/com/example/vanda/agrivolution/Encyclopedia.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 
 import android.support.v7.widget.RecyclerView;
@@ -23,9 +26,11 @@ import android.widget.Toast;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.firebase.ui.database.SnapshotParser;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 public class Encyclopedia extends AppCompatActivity
@@ -34,13 +39,33 @@ public class Encyclopedia extends AppCompatActivity
     private Button btnBack;
     private DatabaseReference mDatabase;
     private FirebaseRecyclerAdapter firebaseRecyclerAdapter;
-
+    private String userID;
+    private FirebaseAuth firebaseauthObj;
     private RecyclerView mPestList;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_encyclopedia);
+        firebaseauthObj = FirebaseAuth.getInstance();
+        FirebaseUser user = firebaseauthObj.getCurrentUser();
+        userID = user.getUid();
+
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("User").child(userID);
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                UserProfile userProf = dataSnapshot.getValue(UserProfile.class);
+                String userType = userProf.getUserType();
+                if(userType.equals("Farmer")){
+                    Add.setVisibility(View.GONE);
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
         Add = findViewById(R.id.buttonAdd);
         mDatabase = FirebaseDatabase.getInstance().getReference().child("Encyclopedia");
